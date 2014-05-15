f4e865a135d5d3118eb943aafa8af54434f2446a
Harenson Henao <harenson@gmail.com>
2014-05-15 18:00:04 -0500

    Abstract messages away with the help of gen_server

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # {Key, Value} cache
 ### TODO
 1. Add option for changing the value of an existing key.
-2. Abstract messages away with the help of functions dealing with receiving and sending them:
+2. ~~Abstract messages away with the help of functions dealing with receiving and sending them:~~ ** % Done using gen_server**
       * start/0 -> () -> avoid user do this `Cache = spawn(cache, process, [[]]).`
       * store/2 -> (Pid, {NewKey, NewValue})
       * update/2 -> (Pid, {ExistingKey, NewValue}) (first point must be done first)
@@ -11,36 +11,29 @@
 
 ### How to tests cache.erl module (manually)
 ```
-39> c(cache).
+1> c(cache).
 {ok,cache}
-40> f().
-ok
-41> Cache = spawn(cache, process, [[]]).
-<0.110.0>
-42> Cache ! {self(), store, {foo, 123}}.
-{<0.33.0>,store,{foo,123}}
-43> flush().
-Shell got {stored,{foo,123}}
-ok
-44> Cache ! {self(), store, {foo, 123}}.
-{<0.33.0>,store,{foo,123}}
-45> flush().
-Shell got already_exist
-Shell got {foo,123}
-ok
-46> Cache ! {self(), lookup, {foo}}.
-{<0.33.0>,lookup,{foo}}
-47> flush().
-Shell got 123
-ok
-48> Cache ! {self(), whatever}.
-{<0.33.0>,whatever}
-49> flush().
-Shell got unknown_request
-ok
-50> Cache ! {self(), list}.
-{<0.33.0>,list}
-51> flush().
-Shell got [{foo,123}]
+2> cache:start_link().
+{ok,<0.41.0>}
+3> gen_server:call(cache, {store, {foo, 123}}).
+{stored,{foo,123}}
+4> gen_server:call(cache, {store, {bar, 456}}).
+{stored,{bar,456}}
+5> gen_server:call(cache, {store, {baz, 789}}).
+{stored,{baz,789}}
+6> gen_server:call(cache, {store, {baz, 789}}).
+{already_exist,{baz,789}}
+7> gen_server:call(cache, {lookup, {bar}}).
+{ok,456}
+8> gen_server:call(cache, {lookup, {foo}}).
+{ok,123}
+9> gen_server:call(cache, {lookup, {baz}}).
+{ok,789}
+10> gen_server:call(cache, unknown_request).
+{error,{unknown_request,unknown_request}}
+11> gen_server:call(cache, my_request).
+{error,{unknown_request,my_request}}
+12> gen_server:cast(cache, list).
+Cache content: [{baz,789},{bar,456},{foo,123}]
 ok
 ```
--- cache.erl
+++ cache.erl
@@ -1,29 +1,51 @@
 -module(cache).
--export([process/1]).
+-behavior(gen_server).
+-export([start_link/0]).
+%% gen_server callbacks
+-export([init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3]).
 
-process(CacheList) ->
-    receive
-        {Pid, store, {Key, Value}} ->
-            case lists:keyfind(Key, 1, CacheList) of
-                false ->
-                    NewCacheList = [{Key, Value} | CacheList],
-                    Pid ! {stored, {Key, Value}},
-                    process(NewCacheList);
-                {ExistingKey, ExistingValue} ->
-                    Pid ! already_exist,
-                    Pid ! {ExistingKey, ExistingValue},
-                    process(CacheList)
-            end;
-        {Pid, lookup, {Key}} ->
-            Pid ! case lists:keyfind(Key, 1, CacheList) of
-                {Key, Value} -> Value;
-                false -> not_found
-            end,
-            process(CacheList);
-        {Pid, list} ->
-            Pid ! CacheList,
-            process(CacheList);
-        {Pid, _} ->
-            Pid ! unknown_request,
-            process(CacheList)
-    end.
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+init(CacheList) ->
+    {ok, CacheList}.
+
+handle_call({store, {Key, Value}}, _From, CacheList) ->
+    case lists:keyfind(Key, 1, CacheList) of
+        false ->
+            NewCacheList = [{Key, Value} | CacheList],
+            Reply = {stored, {Key, Value}},
+            {reply, Reply, NewCacheList};
+        {ExistingKey, ExistingValue} ->
+            Reply = {already_exist, {ExistingKey, ExistingValue}},
+            {reply, Reply, CacheList}
+    end;
+
+handle_call({lookup, {Key}}, _From, CacheList) ->
+    Reply = case lists:keyfind(Key, 1, CacheList) of
+        {Key, Value} -> Value;
+        false -> not_found
+    end,
+    {reply, {ok, Reply}, CacheList};
+
+handle_call(_Unknown, _From, CacheList) ->
+    Reply = {unknown_request, _Unknown},
+    {reply, {error, Reply}, CacheList}.
+
+handle_cast(list, CacheList) ->
+    io:format("Cache content: ~w~n", [CacheList]),
+    {noreply, CacheList}.
+
+handle_info(_Request, CacheList) ->
+    {noreply, CacheList}.
+
+terminate(_Reason, _CacheList) ->
+    {ok, _Reason}.
+
+code_change(_OldVsn, CacheList, _Extra) ->
+    {ok, CacheList}.
