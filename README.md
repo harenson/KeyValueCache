7e512b7b6931cad0a269568bf2b9d83fc14aa041
Harenson Henao <harenson@gmail.com>
2014-05-16 18:11:18 -0500

    Abstract messages away with the help of functions dealing with receiving and sending them

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # {Key, Value} cache
 ### TODO
-1. Add option for changing the value of an existing key.
+1. ~~Add option for changing the value of an existing key.~~
 2. ~~Abstract messages away with the help of functions dealing with receiving and sending them:~~ **% Done using gen_server**
-      * start/0 -> () -> avoid user do this `Cache = spawn(cache, process, [[]]).`
-      * store/2 -> (Pid, {NewKey, NewValue})
-      * update/2 -> (Pid, {ExistingKey, NewValue}) (first point must be done first)
-      * lookup/2 -> (Pid, {ExistingKey})
-      * list/1 -> (Pid)
+      * ~~start_link/0 -> () -> avoid user do this `Cache = spawn(cache, process, [[]]).`~~
+      * ~~store/2 -> (NewKey, NewValue)~~
+      * ~~update/2 -> (ExistingKey, NewValue) (first point must be done first)~~
+      * ~~lookup/1 -> (ExistingKey)~~
+      * ~~list/0 -> ()~~
 3. Add Unit Tests
 
 ### How to tests cache.erl module (manually)
@@ -15,25 +15,20 @@
 {ok,cache}
 2> cache:start_link().
 {ok,<0.41.0>}
-3> gen_server:call(cache, {store, {foo, 123}}).
-{stored,{foo,123}}
-4> gen_server:call(cache, {store, {bar, 456}}).
-{stored,{bar,456}}
-5> gen_server:call(cache, {store, {baz, 789}}).
-{stored,{baz,789}}
-6> gen_server:call(cache, {store, {baz, 789}}).
-{already_exist,{baz,789}}
-7> gen_server:call(cache, {lookup, {bar}}).
-{ok,456}
-8> gen_server:call(cache, {lookup, {foo}}).
-{ok,123}
-9> gen_server:call(cache, {lookup, {baz}}).
-{ok,789}
-10> gen_server:call(cache, unknown_request).
-{error,{unknown_request,unknown_request}}
-11> gen_server:call(cache, my_request).
-{error,{unknown_request,my_request}}
-12> gen_server:cast(cache, list).
-Cache content: [{baz,789},{bar,456},{foo,123}]
+3> cache:store(foo, 123).
+{ok,stored,{foo,123}}
+4> cache:store(bar, 456).
+{ok,stored,{bar,456}}
+5> cache:list().
+Cache content: [{bar,456},{foo,123}]
 ok
+6> cache:update(bar, 789).
+{ok,updated,{bar,789}}
+7> cache:list().
+Cache content: [{bar,789},{foo,123}]
+ok
+8> cache:lookup(foo).
+{ok,123}
+9> cache:lookup(foobar).
+{error,{key_not_found,foobar}}
 ```
--- cache.erl
+++ cache.erl
@@ -1,6 +1,11 @@
 -module(cache).
 -behavior(gen_server).
--export([start_link/0]).
+%% API
+-export([start_link/0,
+         store/2,
+         update/2,
+         lookup/1,
+         list/0]).
 %% gen_server callbacks
 -export([init/1,
          handle_call/3,
@@ -15,26 +20,34 @@ start_link() ->
 init(CacheList) ->
     {ok, CacheList}.
 
-handle_call({store, {Key, Value}}, _From, CacheList) ->
+handle_call({store, {Key, Value}, Overwrite}, _From, CacheList) ->
     case lists:keyfind(Key, 1, CacheList) of
         false ->
             NewCacheList = [{Key, Value} | CacheList],
-            Reply = {stored, {Key, Value}},
+            Reply = {ok, stored, {Key, Value}},
             {reply, Reply, NewCacheList};
         {ExistingKey, ExistingValue} ->
-            Reply = {already_exist, {ExistingKey, ExistingValue}},
-            {reply, Reply, CacheList}
+            if
+                Overwrite =:= true -> % Updates an existing Key with a new Value
+                    TmpList = lists:delete({ExistingKey, ExistingValue}, CacheList),
+                    NewCacheList = [{Key, Value} | TmpList],
+                    Reply = {ok, updated, {Key, Value}},
+                    {reply, Reply, NewCacheList};
+                true ->
+                    Reply = {error, key_already_exist, {ExistingKey, ExistingValue}},
+                    {reply, Reply, CacheList}
+            end
     end;
 
 handle_call({lookup, {Key}}, _From, CacheList) ->
     Reply = case lists:keyfind(Key, 1, CacheList) of
-        {Key, Value} -> Value;
-        false -> not_found
+        {Key, Value} -> {ok, Value};
+        false -> {error, {key_not_found, Key}}
     end,
-    {reply, {ok, Reply}, CacheList};
+    {reply, Reply, CacheList};
 
-handle_call(_Unknown, _From, CacheList) ->
-    Reply = {unknown_request, _Unknown},
+handle_call(Unknown, _From, CacheList) ->
+    Reply = {unknown_request, Unknown},
     {reply, {error, Reply}, CacheList}.
 
 handle_cast(list, CacheList) ->
@@ -49,3 +62,23 @@ terminate(_Reason, _CacheList) ->
 
 code_change(_OldVsn, CacheList, _Extra) ->
     {ok, CacheList}.
+
+
+%% API
+
+% Store a new {Key, Value} pair in the cache.
+store(Key, Value) ->
+    gen_server:call(?MODULE, {store, {Key, Value}, false}).
+
+% Update and existing {Key, Value} pair with the NewValue or
+% create a new one if {Key, NewValue} pair doesn't exist
+update(Key, NewValue) ->
+    gen_server:call(?MODULE, {store, {Key, NewValue}, true}).
+
+% Lookup for Key in the cache and return its value.
+lookup(Key) ->
+    gen_server:call(?MODULE, {lookup, {Key}}).
+
+% List the cache content.
+list() ->
+    gen_server:cast(?MODULE, list).
