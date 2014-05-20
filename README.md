969dfdac347674e50d5323fd6ea9172cacba48ca
Harenson Henao <harenson@gmail.com>
2014-05-20 13:28:59 -0500

    Add time management

--- README.md
+++ README.md
@@ -8,28 +8,28 @@
       * ~~lookup/1 -> (ExistingKey)~~
       * ~~list/0 -> ()~~
 3. ~~Add Unit Tests~~
-4. Add time management
+4. ~~Add time management~~
 
 ### How to tests cache.erl module (manually)
 ```
 1> c(cache).
 {ok,cache}
 2> cache:start_link().
-{ok,<0.41.0>}
+{ok,{interval,#Ref<0.0.0.2067>}}
 3> cache:store(foo, 123).
-{ok,stored,{foo,123}}
+{ok,stored,{foo,123,1400610207}}
 4> cache:store(bar, 456).
-{ok,stored,{bar,456}}
+{ok,stored,{bar,456,1400610217}}
 5> cache:list().
-Cache content: [{bar,456},{foo,123}]
-ok
+{ok,[{bar,456,1400610217},{foo,123,1400610207}]}
 6> cache:update(bar, 789).
 {ok,updated,{bar,789}}
 7> cache:list().
-Cache content: [{bar,789},{foo,123}]
-ok
+{ok,[{bar,789,1400610217},{foo,123,1400610207}]}
 8> cache:lookup(foo).
-{ok,123}
+{ok,123,1400610207}
 9> cache:lookup(foobar).
 {error,{key_not_found,foobar}}
+10> cache:lookup(foo).   
+{error,{key_not_found,foo}} %% Cache time expired (comment added manually)
 ```
--- cache.erl
+++ cache.erl
@@ -7,6 +7,8 @@
          lookup/1,
          list/0,
          stop/0]).
+%% Internal functions
+-export([clean_cache/0]).
 %% gen_server callbacks
 -export([init/1,
          handle_call/3,
@@ -16,33 +18,52 @@
          code_change/3]).
 
 start_link() ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []),
+    %% Calls ?MODULE:clean_cache/0 function every x milliseconds.
+    timer:apply_interval(2000, ?MODULE, clean_cache, []).
 
 init(CacheList) ->
     {ok, CacheList}.
 
 handle_call({store, {Key, Value}, Overwrite}, _From, CacheList) ->
     case lists:keyfind(Key, 1, CacheList) of
+        %% Key doesn't exists in the cache yet.
         false ->
-            NewCacheList = [{Key, Value} | CacheList],
-            Reply = {ok, stored, {Key, Value}},
+            %% Sets Expires time for this
+            %% {Key, Value} pair in the cache.
+            Expires = timestamp() + 60,
+            NewCacheList = [{Key, Value, Expires} | CacheList],
+            Reply = {ok, stored, {Key, Value, Expires}},
             {reply, Reply, NewCacheList};
-        {ExistingKey, ExistingValue} ->
+        %% Key already exists in the cache.
+        {ExistingKey, ExistingValue, ExistingExpires} ->
+            Now = timestamp(),
             if
-                Overwrite =:= true -> % Updates an existing Key with a new Value
-                    TmpList = lists:delete({ExistingKey, ExistingValue}, CacheList),
-                    NewCacheList = [{Key, Value} | TmpList],
+                Now > ExistingExpires ->
+                    Reply = {error,
+                             expired,
+                             {ExistingKey, ExistingValue, ExistingExpires}},
+                    {reply, Reply, CacheList};
+                %% Updates an existing Key with a new Value.
+                Overwrite =:= true ->
+                    TmpList = lists:delete({ExistingKey,
+                                            ExistingValue,
+                                            ExistingExpires},
+                                           CacheList),
+                    NewCacheList = [{Key, Value, ExistingExpires} | TmpList],
                     Reply = {ok, updated, {Key, Value}},
                     {reply, Reply, NewCacheList};
                 true ->
-                    Reply = {error, key_already_exist, {ExistingKey, ExistingValue}},
+                    Reply = {error,
+                             key_already_exist,
+                             {ExistingKey, ExistingValue, ExistingExpires}},
                     {reply, Reply, CacheList}
             end
     end;
 
 handle_call({lookup, {Key}}, _From, CacheList) ->
     Reply = case lists:keyfind(Key, 1, CacheList) of
-        {Key, Value} -> {ok, Value};
+        {Key, Value, Expires} -> {ok, Value, Expires};
         false -> {error, {key_not_found, Key}}
     end,
     {reply, Reply, CacheList};
@@ -50,12 +71,22 @@ handle_call({lookup, {Key}}, _From, CacheList) ->
 handle_call(stop, _From, CacheList) ->
     {stop, normal, ok, CacheList};
 
+handle_call(list, _From, CacheList) ->
+    Reply = {ok, CacheList},
+    {reply, Reply, CacheList};
+
 handle_call(Unknown, _From, CacheList) ->
     Reply = {unknown_request, Unknown},
     {reply, {error, Reply}, CacheList}.
 
-handle_cast(list, CacheList) ->
-    io:format("Cache content: ~w~n", [CacheList]),
+handle_cast(clean_cache, CacheList) ->
+    %% {K, V, E} = {Key, Value, Expires}
+    %io:format("[~p] Cleaning cache~n", [timestamp()]), %% Debug
+    Now = timestamp(),
+    CleanedCacheList = [{K, V, E} || {K, V, E} <- CacheList, E > Now],
+    {noreply, CleanedCacheList};
+
+handle_cast(_, CacheList) ->
     {noreply, CacheList}.
 
 handle_info(_Request, CacheList) ->
@@ -68,25 +99,38 @@ code_change(_OldVsn, CacheList, _Extra) ->
     {ok, CacheList}.
 
 
-%% API
+%%% API
 
-% Store a new {Key, Value} pair in the cache.
+%% Store a new {Key, Value} pair in the cache.
 store(Key, Value) ->
     gen_server:call(?MODULE, {store, {Key, Value}, false}).
 
-% Update and existing {Key, Value} pair with the NewValue or
-% create a new one if {Key, NewValue} pair doesn't exist
+%% Update and existing {Key, Value} pair with the NewValue or
+%% create a new one if {Key, NewValue} pair doesn't exist.
 update(Key, NewValue) ->
     gen_server:call(?MODULE, {store, {Key, NewValue}, true}).
 
-% Lookup for Key in the cache and return its value.
+%% Lookup for Key in the cache and return its value.
 lookup(Key) ->
     gen_server:call(?MODULE, {lookup, {Key}}).
 
-% List the cache content.
+%% List the cache content.
 list() ->
-    gen_server:cast(?MODULE, list).
+    gen_server:call(?MODULE, list).
 
-% Stop the process
+%% Stop the process.
 stop() ->
     gen_server:call(?MODULE, stop).
+
+
+%%% Internal functions
+
+%% Returns the current date and time in seconds.
+timestamp() ->
+    {MegaSecs, Secs, _} = os:timestamp(),
+    {NowTimestamp, _} = string:to_integer(lists:concat([MegaSecs, Secs])),
+    NowTimestamp.
+
+%% Removes expired entries from the cache list.
+clean_cache() ->
+    gen_server:cast(?MODULE, clean_cache).
